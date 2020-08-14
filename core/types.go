46aa93d6878aa73809ed16dab0c32e71ba9139a2
TwinProduction <twin@twinnation.org>
2020-08-13 23:20:22 -0400

    Remove useless HealthStatus struct

--- core/types.go
+++
@@ -1,6 +0,0 @@
-package core
-
-type HealthStatus struct {
-	Status  string `json:"status"`
-	Message string `json:"message,omitempty"`
-}
--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"github.com/TwinProduction/gatic/core"
 	"log"
 	"net/http"
 )
@@ -10,19 +8,11 @@ import (
 func main() {
 	http.HandleFunc("/health", healthHandler)
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-	log.Println("[main][main] Listening on port 8080")
+	log.Println("[main] Listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func healthHandler(writer http.ResponseWriter, _ *http.Request) {
 	writer.WriteHeader(http.StatusOK)
-	_, _ = writer.Write(structToJsonBytes(&core.HealthStatus{Status: "UP"}))
-}
-
-func structToJsonBytes(obj interface{}) []byte {
-	bytes, err := json.Marshal(obj)
-	if err != nil {
-		log.Printf("[main][structToJsonBytes] Unable to marshall object to JSON: %s", err.Error())
-	}
-	return bytes
+	_, _ = writer.Write([]byte("{\"status\":\"UP\"}"))
 }
